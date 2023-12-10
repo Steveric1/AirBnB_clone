d7f635ecd0226de4e450456e64ec8a0d9a602eb5
Navygeta <tinashenavonika@gmail.com>
2023-12-10 20:00:54 +0200

    Console update and show fix

--- console.py
+++ console.py
@@ -15,8 +15,8 @@ from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """Custom (hbnb cmd prompt) using the cmd module"""
-    
-    __classess = {
+
+    __classes = {
         "BaseModel",
         "User",
         "State",
@@ -25,6 +25,7 @@ class HBNBCommand(cmd.Cmd):
         "Place",
         "Review"
     }
+
     def __init__(self):
         super().__init__()
         self.prompt = "(hbnb) "
@@ -41,18 +42,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Empty line + enter implementation of doing nothing"""
         pass
-    
+
     def do_create(self, arg):
         my_args = arg.split()
         if not arg:
             print("** class name missing **")
-        elif my_args[0] not in HBNBCommand.__classess:
+        elif my_args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
             new_inst = eval(my_args[0])()
             new_inst.save()
             print(new_inst.id)
-    
+
     def do_show(self, arg):
         """Show created id"""
         my_args = arg.split()
@@ -60,7 +61,7 @@ class HBNBCommand(cmd.Cmd):
         try:
             if not arg:
                 raise ValueError("** class name missing **")
-            elif my_args[0] not in HBNBCommand.__classess:
+            elif my_args[0] not in HBNBCommand.__classes:
                 raise ValueError("** class doesn't exist **")
             elif len(my_args) < 2:
                 raise ValueError("** instance id missing **")
@@ -72,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                     print(create_inst[keys])
         except ValueError as e:
             print(e)
-        
+
     def do_destroy(self, arg):
         """Delete method"""
         my_args = arg.split()
@@ -80,7 +81,7 @@ class HBNBCommand(cmd.Cmd):
         try:
             if not arg:
                 raise ValueError("** class name missing **")
-            elif my_args[0] not in HBNBCommand.__classess:
+            elif my_args[0] not in HBNBCommand.__classes:
                 raise ValueError("** class doesn't exist **")
             elif len(my_args) < 2:
                 raise ValueError("** instance id missing **")
@@ -93,22 +94,71 @@ class HBNBCommand(cmd.Cmd):
                     storage.save()
         except ValueError as e:
             print(e)
-    
+
     def do_all(self, args):
         """all method"""
         my_args = args.split()
         all_inst = []
-        
-        if len(my_args) > 0 and my_args[0] not in HBNBCommand.__classess:
-            print("**class name doesn't exist**")
+
+        if len(my_args) > 0 and my_args[0] not in HBNBCommand.__classes:
+            print("** class name doesn't exist **")
         else:
-           all_inst = []
-           for all in storage.all().values():
-               if len(my_args) > 0 and my_args[0] == all.__class__.__name__:
-                   all_inst.append(all.__str__())
-               elif len(my_args) == 0:
-                   all_inst.append(all.__str__())
-           print(all_inst)
-            
+            all_inst = []
+            for all_instance in storage.all().values():
+                if (len(my_args) > 0 and
+                        my_args[0] == all_instance.__class__.__name__):
+                    all_inst.append(all_instance.__str__())
+
+                elif len(my_args) == 0:
+                    all_inst.append(all_instance.__str__())
+            print(all_inst)
+
+    def do_update(self, arg):
+        """Updates an instance"""
+        my_args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+
+        _class = my_args[0]
+        if _class not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(my_args) < 2:
+            print("** instance id missing **")
+            return
+
+        inst_id = my_args[1]
+        keys = "{}.{}".format(_class, inst_id)
+        all_inst = storage.all()
+
+        if keys not in all_inst:
+            print("** no instance found **")
+            return
+
+        if len(my_args) < 3:
+            print("** attribute name missing **")
+            return
+
+        attr_name = my_args[2]
+        if len(my_args) < 4:
+            print("** value missing **")
+            return
+
+        value = my_args[3]
+
+        _inst = all_inst[keys]
+        if hasattr(_inst, attr_name):
+            attr_type = type(getattr(_inst, attr_name))
+            try:
+                setattr(_inst, attr_name, attr_type(value))
+                _inst.save()
+            except (ValueError, TypeError):
+                print("** invalid value **")
+        else:
+            print("** attribute doesn't exist **")
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
