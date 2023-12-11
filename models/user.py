79fbfabaf3416eafb388c6ef69090ec06e3c8eb1
Eric <Stephen.erich243@gmail.com>
2023-12-11 19:58:21 +0100

    updated

--- models/user.py
+++ models/user.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 from models.base_model import BaseModel
+import json
 
 
 class User(BaseModel):
-    def __init__(self, email, password, first_name, last_name, **kwargs):
-        super().__init__(**kwargs)
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+    '''subclass of BaseModel class'''
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
