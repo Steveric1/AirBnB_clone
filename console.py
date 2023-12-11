0030415bd290adba17745f7306b7c8480fd8bed1
Eric <Stephen.erich243@gmail.com>
2023-12-11 23:07:59 +0100

    updated

--- console.py
+++ console.py
@@ -44,51 +44,51 @@ class HBNBCommand(cmd.Cmd):
         """Empty line + enter implementation of doing nothing"""
         pass
 
-    def do_create(self, line):
-        """Creates a new instance of BaseModel and saves it to json file"""
-        args = line.split()
-        if not args[0]:
-            print("** class name missing **")
-        elif args[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        else:
-            instance = eval(args[0])()
-            instance.save()
-            print(instance.id)
+    # def do_create(self, line):
+    #     """Creates a new instance of BaseModel and saves it to json file"""
+    #     args = line.split()
+    #     if not args[0]:
+    #         print("** class name missing **")
+    #     elif args[0] not in HBNBCommand.__classes:
+    #         print("** class doesn't exist **")
+    #     else:
+    #         instance = eval(args[0])()
+    #         instance.save()
+    #         print(instance.id)
 
-#     def do_create(self, line):
-#         """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
-#         Create a new class instance with given keys/values and print its id.
-#         """
-#         try:
-#             if not line:
-#                 raise SyntaxError()
-#             my_list = line.split(" ")
-#
-#             kwargs = {}
-#             for i in range(1, len(my_list)):
-#                 key, value = tuple(my_list[i].split("="))
-#                 if value[0] == '" "':
-#                     value = value.strip('" "').replace("_", " ")
-#                 else:
-#                     try:
-#                         value = eval(value)
-#                     except (SyntaxError, NameError):
-#                         continue
-#                 kwargs[key] = value
-#
-#             if kwargs == {}:
-#                 obj = eval(my_list[0])()
-#             else:
-#                 obj = eval(my_list[0])(**kwargs)
-#                 storage.new(obj)
-#             print(obj.id)
-#             obj.save()
-#
-#         except SyntaxError:
-#             print("** class name missing **")
-#         except NameError:
-#             print("** class doesn't exist **")
+    def do_create(self, line):
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
+        try:
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '" "':
+                    value = value.strip('" "').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
 
     def do_show(self, arg):
         """Show created id"""
